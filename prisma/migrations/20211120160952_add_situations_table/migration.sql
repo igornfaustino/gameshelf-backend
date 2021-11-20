77cacb52d5300bf2a012c75c6bf91cc8ba706b09
igornfaustino <igor@nfaustino.com>
2021-11-20 13:18:27 -0300

    feat: add situation table

---
+++ prisma/migrations/20211120160952_add_situations_table/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "situations" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "situations_pkey" PRIMARY KEY ("id")
+);
--- scripts/setupInitialData.ts
+++ scripts/setupInitialData.ts
@@ -1,10 +1,16 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-await-in-loop */
 import { getGenres, getPlatforms } from '../src/connectors/igdb';
+import { prisma } from '../src/db';
 import { requestAccessToken } from '../src/helpers/request';
 import { createOrUpdateGenre } from '../src/modules/games/controllers/GenreController';
 import { saveOrUpdatePlatform } from '../src/modules/games/controllers/PlatformController';
 import { saveIgdbToken } from '../src/modules/shared/controllers/app';
+
+const toPlay = 'to play';
+const playing = 'playing';
+const completed = 'completed';
+const abandoned = 'abandoned';
 
 export const setupInitialData = async () => {
 	const res = await requestAccessToken();
@@ -19,6 +25,27 @@ export const setupInitialData = async () => {
 	for (const platform of platforms) {
 		await saveOrUpdatePlatform(platform);
 	}
+
+	await prisma.situations.upsert({
+		where: { name: toPlay },
+		create: { name: toPlay },
+		update: { name: toPlay },
+	});
+	await prisma.situations.upsert({
+		where: { name: playing },
+		create: { name: playing },
+		update: { name: playing },
+	});
+	await prisma.situations.upsert({
+		where: { name: completed },
+		create: { name: completed },
+		update: { name: completed },
+	});
+	await prisma.situations.upsert({
+		where: { name: abandoned },
+		create: { name: abandoned },
+		update: { name: abandoned },
+	});
 };
 
 setupInitialData();
